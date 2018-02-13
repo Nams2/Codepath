f161ca750114d7dc8699f3dbcc1c2692587f3a2c
Nams2 <namratab_03@yahoo.com>
2018-02-12 22:41:13 -0800

    Update URLInfo.java

--- PreWork/URL/URLInfo.java
+++ PreWork/URL/URLInfo.java
@@ -1,17 +1,11 @@
 
 public class URLInfo {
 	String url;
-	Integer redirectType = 302;
 	String shortUrl;
-	int counter = 0;
-	String accountId;
 
-	public URLInfo(String url, Integer redirectType, String shortUrl, int counter, String accountId) {
+	public URLInfo(String url, String shortUrl) {
 		this.url = url;
-		this.redirectType = redirectType;
 		this.shortUrl = shortUrl;
-		this.counter = counter;
-		this.accountId = accountId;
 	}
 
 	public String getUrl() {
@@ -22,35 +16,11 @@ public class URLInfo {
 		this.url = url;
 	}
 
-	public Integer getRedirectType() {
-		return redirectType;
-	}
-
-	public void setRedirectType(Integer redirectType) {
-		this.redirectType = redirectType;
-	}
-
 	public String getShortUrl() {
 		return shortUrl;
 	}
 
 	public void setShortUrl(String shortUrl) {
 		this.shortUrl = shortUrl;
-	}
-
-	public int getCounter() {
-		return counter;
-	}
-
-	public void setCounter(int counter) {
-		this.counter = counter;
-	}
-
-	public String getAccountId() {
-		return accountId;
-	}
-
-	public void setAccountId(String accountId) {
-		this.accountId = accountId;
 	}
 }
